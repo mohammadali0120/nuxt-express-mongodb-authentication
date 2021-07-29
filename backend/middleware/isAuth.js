d743946c49f1de55e361f38c6685128095b8888e
MohammadAli Khaksar <59791091+mohammadali0120@users.noreply.github.com>
2021-07-29 14:11:50 +0430

    Update isAuth.js

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
   const token = authHeader.split(" ")[1];
   let decodedToken;
   try {
-    decodedToken = jwt.verify(token, "somesupersecretsecret");
+    decodedToken = jwt.verify(token, "expressnuxtsecret");
   } catch (err) {
     err.statusCode = 500;
     throw err;
