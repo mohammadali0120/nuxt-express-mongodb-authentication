d6a300f95df9a01b006bb4968faab7c08f47596a
MohammadAli Khaksar <59791091+mohammadali0120@users.noreply.github.com>
2021-09-05 14:58:15 +0430

    Update nuxt.config.js

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -45,8 +45,13 @@ export default {
           property: "user",
           autoFetch: true
         },
+//      refreshToken: {  // it sends request automatically when the access token expires, and its expire time has set on the Back-end and does not need to we set it here, because is useless
+//        property: "refresh_token",
+//        data: "refresh_token",
+//      },
         endpoints: {
           login: { url: "/api/auth/login", method: "post" },
+//        refresh: { url: "/api/auth/refresh-token", method: "post" },
           logout: false, //  we don't have an endpoint for our logout in our API and we just remove the token from localstorage
           user: { url: "/api/auth/user", method: "get" }
         }
